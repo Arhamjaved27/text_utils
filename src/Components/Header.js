0ba248fb176ce3e01338615a60513237f84c8aa7
Arham <arhamjaved867@gmail.com>
2024-09-29 21:31:59 +0500

    TextUtils functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,52 @@
 import React from 'react'
 import '../App.css';
-
+import {useRef} from 'react';
+import {useState} from 'react';
 
 export default function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const togelOpen = useRef(null);
+  const togelClose = useRef(null);
+  const dorpMenu = useRef(null);
+
+
+  const DropdownMenu = ()=>{
+
+    if(menuOpen === false)
+    {
+      console.log("open")
+      togelOpen.current.style.display = 'none';
+      
+      togelClose.current.style.display = 'block';
+      
+      dorpMenu.current.style.height = '0px';
+      dorpMenu.current.style.opacity = '0';
+      dorpMenu.current.style.overflow = 'hidden';
+      dorpMenu.current.style.transition = 'all .3s ease-in-out';
+
+
+      setMenuOpen(true);
+    }
+    else
+    {
+      console.log("Close")
+      togelOpen.current.style.display = 'block';
+      
+      togelClose.current.style.display = 'none';
+      
+      dorpMenu.current.style.opacity = '1';
+      dorpMenu.current.style.height = '220px';
+
+
+      setMenuOpen(false);
+    }
+
+    
+  }
+
+
+
   return (
     <div className='contaner bg-dark p-3 d-flex align-items-center justify-content-between ps-5 pe-5 position-relative' style={{height:'80px'}}>
       <h1 className='text-white  me-3'>Arham</h1>
@@ -20,18 +64,17 @@ export default function Header() {
         <input type='search' id='srch' className=''/>
         <button className='mb-1 ms-2 btn bg-secondary-color pe-4  ps-4 btn-hover'>Search</button>
        </div>
-      <div id="drop-togle" className='position-relative bg-secondary-color' style={{width:'40px', height:'40px'}}>
-        <i id='togle-open' class="bi bi-list position-absolute top-0" style={{fontSize:'30px'}} ></i>
-        <i id='togel-close' class="bi bi-x-lg " style={{fontSize:'30px'}}></i>
+      <div id="drop-togle" className='position-relative bg-secondary-color' style={{width:'40px', height:'40px'}} onClick={DropdownMenu}>
+        <i ref={togelOpen}  id='togle-open' class="bi bi-list position-absolute top-0" style={{fontSize:'30px'}} ></i>
+        <i ref={togelClose} id='togel-close' class="bi bi-x-lg " style={{fontSize:'30px'}}></i>
         
       </div>
-
-      <div id="drop-menu" className='d-none bg-primary position-absolute top-100 end-0 me-1 wv-10'  style={{height:'210px', boxSizing:'border-box'}}>
+      <div ref={dorpMenu} id="drop-menu" className='bg-secondary-color position-absolute top-100 start-0 pt-2 wv-10 rounded'>
         <ul className='d-flex flex-column align-items-center text-white list-unstyled'>
-            <li className='drop-link'> <a href="/" className='link-cl '> Home </a></li>
-            <li className='drop-link'> <a href="/" className='link-cl '> About </a></li>
-            <li className='drop-link'> <a href="/" className='link-cl '> Projects </a></li>
-            <li className='drop-link'> <a href="/" className='link-cl'> Services </a></li>
+            <li className='drop-link'> <a href="/" className='down-link-cl'> Home </a></li>
+            <li className='drop-link'> <a href="/" className='down-link-cl'> About </a></li>
+            <li className='drop-link'> <a href="/" className='down-link-cl'> Projects </a></li>
+            <li className='drop-link'> <a href="/" className='down-link-cl'> Services </a></li>
           </ul>
         </div>
     </div>
--- src/Components/TextUtils.js
+++ src/Components/TextUtils.js
@@ -1,24 +1,43 @@
 import React from 'react'
 import '../Universal.css';
+import { useState } from 'react';
 
 export default function TextUtils() {
+
+    const [text, setText] = useState(0);
+    const [totalLatters, setTotalLatters] = useState(0);
+   
+    const textBox= (event)=>{
+        setText(event.target.value);
+    }
+
+    const  OnUpHangle = ()=>{
+        console.log("Upper case text change");
+        let newText = text.toUpperCase(); 
+        setText(newText);
+        
+    }
+
+    
   return (
     <div className='bg-dark-color hv-9 pt-3'>
         <div id="text-box" className='d-flex flex-column align-items-center' >
-            <textarea name="" id="" rows={12} className='wv-7 p-3 br-5 border-primary-6' placeholder='Enter your text here.... '></textarea>
+            <h3 className='ms-5 txt-primary-color'>Enter Text Below To Analys</h3>
+            <textarea value={text} name="" id="" rows={12} className='wv-7 p-3 br-5 border-primary-6 txt-color-dark' placeholder='Enter your text here.... ' onChange={textBox}></textarea>
         
         <div id="text-btns" className='m-3'>
-            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Capitalize</button>
-            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Capitalize</button>
-            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Capitalize</button>
-            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Capitalize</button>
-            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Capitalize</button>
+            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover' onClick={OnUpHangle} >Upper Case</button>
+            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Lower Case</button>
+            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Clear Text</button>
+            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Copy Text</button>
+            <button type="button" className='btn bg-primary-color text-white me-2 mt-2 btn-hover'>Remove Extra Spaces</button>
         </div>
         
         </div>
 
         <div id="readings" className='ms-5'>
-            <h5>Total number of Characters: </h5>
+            <h5 className='d-inline'></h5>
+            <p> <strong className='txt-color-prinary'>Total number of Characters:</strong> {text} </p>
         </div>
         
     </div>
